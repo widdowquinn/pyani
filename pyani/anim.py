a35303ac7d8460354f7d693ef28faa712abd763f
widdowquinn <leightonpritchard@mac.com>
2015-10-31 16:37:39 +0000

    anim.py: Changes for Python3

--- pyani/anim.py
+++ pyani/anim.py
@@ -26,9 +26,9 @@ import pandas as pd
 import os
 import sys
 
-import pyani_config
-import pyani_files
-import pyani_jobs
+from . import pyani_config
+from . import pyani_files
+from . import pyani_jobs
 
 # Generate list of Job objects, one per NUCmer run
 def generate_nucmer_jobs(filenames, outdir='.',
@@ -139,7 +139,7 @@ def process_deltadir(delta_dir, org_lengths, logger=None):
     """
     # Process directory to identify input files
     deltafiles = pyani_files.get_input_files(delta_dir, '.delta')
-    labels = org_lengths.keys()
+    labels = list(org_lengths.keys())
     # Hold data in pandas dataframe
     alignment_lengths = pd.DataFrame(index=labels, columns=labels,
                                      dtype=float)
@@ -150,7 +150,7 @@ def process_deltadir(delta_dir, org_lengths, logger=None):
     alignment_coverage = pd.DataFrame(index=labels, columns=labels,
                                       dtype=float).fillna(1.0)
     # Fill diagonal NA values for alignment_length with org_lengths
-    for org, length in org_lengths.items():
+    for org, length in list(org_lengths.items()):
         alignment_lengths[org][org] = length
     # Process .delta files assuming that the filename format holds:
     # org1_vs_org2.delta
