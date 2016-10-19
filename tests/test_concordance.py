c6eb25039e47fb0b1db26fb69ae14c13e1bc39e2
Leighton Pritchard <leighton.pritchard@hutton.ac.uk>
2016-10-19 21:58:08 +0100

    restore test of ANIm

--- tests/test_concordance.py
+++ tests/test_concordance.py
@@ -206,7 +206,6 @@ def test_aniblastall_concordance():
 
 
 # Test concordance of this code with JSpecies output
-@nottest
 def test_anim_concordance():
     """Test concordance of ANIm method with JSpecies output."""
     # Make/check output directory
