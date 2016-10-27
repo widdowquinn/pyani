ce2d4e87a3fef711447990912886af77a0fc630f
Leighton Pritchard <leighton.pritchard@hutton.ac.uk>
2016-10-27 14:32:31 +0100

    fix code errors suggested by landscape.io

--- average_nucleotide_identity.py
+++ average_nucleotide_identity.py
@@ -395,7 +395,7 @@ def calculate_anim(infiles, org_lengths):
         else:
             logger.info("Running jobs with SGE")
             logger.info("Jobarray group size set to %d", args.sgegroupsize)
-            run_sge.run_dependency_graph(joblist, verbose=args.verbose,
+            run_sge.run_dependency_graph(joblist,
                                          logger=logger,
                                          jgprefix=args.jobprefix,
                                          sgegroupsize=args.sgegroupsize)
