35ee4b95e05e35aae9d35c940e90147744dee76a
Leighton Pritchard <leighton.pritchard@hutton.ac.uk>
2017-03-21 17:18:31 +0000

    modify setup.py to reflect Python 3.6

--- setup.py
+++ setup.py
@@ -61,6 +61,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
         ],
     )
