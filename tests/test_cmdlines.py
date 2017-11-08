9a4713a785fe39fe9696e883b24d8a12b7f78737
Leighton Pritchard <leighton.pritchard@hutton.ac.uk>
2017-11-08 10:58:30 +0000

    update cmdline tests for new ANIm command structure

--- tests/test_cmdlines.py
+++ tests/test_cmdlines.py
@@ -15,25 +15,27 @@ from pyani import anim
 # Test ANIm command-lines
 # One pairwise comparison
 def test_anim_pairwise_basic():
-    """Test generation of basic NUCmer pairwise comparison command.
-    """
-    cmd = anim.construct_nucmer_cmdline("file1.fna", "file2.fna")
-    assert_equal(cmd, "nucmer --mum -p ./nucmer_output/file1_vs_file2 " +
-                 "file1.fna file2.fna; delta-filter -1 " +
-                 "./nucmer_output/file1_vs_file2.delta > " +
+    """Test generation of basic NUCmer pairwise comparison command."""
+    ncmd, fcmd = anim.construct_nucmer_cmdline("file1.fna", "file2.fna")
+    print("\n{}\n{}\n".format(ncmd, fcmd))
+    assert_equal(ncmd, "nucmer --mum -p ./nucmer_output/file1_vs_file2 " +
+                 "file1.fna file2.fna")
+    assert_equal(fcmd, "delta_filter_wrapper.py delta-filter -1 " +
+                 "./nucmer_output/file1_vs_file2.delta " +
                  "./nucmer_output/file1_vs_file2.filter")
-    print(cmd)
+
 
 def test_anim_pairwise_maxmatch():
-    """Test generation of NUCmer pairwise comparison command with maxmatch.
-    """
-    cmd = anim.construct_nucmer_cmdline("file1.fna", "file2.fna",
-                                        maxmatch=True)
-    assert_equal(cmd, "nucmer --maxmatch -p ./nucmer_output/file1_vs_file2 " +
-                 "file1.fna file2.fna; delta-filter -1 " +
-                 "./nucmer_output/file1_vs_file2.delta > "
+    """Test generation of NUCmer pairwise comparison command with maxmatch."""
+    ncmd, fcmd = anim.construct_nucmer_cmdline("file1.fna", "file2.fna",
+                                               maxmatch=True)
+    print("\n{}\n{}\n".format(ncmd, fcmd))
+    assert_equal(ncmd, "nucmer --maxmatch -p ./nucmer_output/file1_vs_file2 " +
+                 "file1.fna file2.fna")
+    assert_equal(fcmd, "delta_filter_wrapper.py delta-filter -1 " +
+                 "./nucmer_output/file1_vs_file2.delta " + 
                  "./nucmer_output/file1_vs_file2.filter")    
-    print(cmd)
+    print("\n{}\n{}\n".format(ncmd, fcmd))
 
 
 # List of pairwise comparisons
@@ -41,29 +43,30 @@ def test_anim_collection():
     """Test generation of list of NUCmer comparison commands.
     """
     files = ["file1", "file2", "file3", "file4"]
-    cmdlist = anim.generate_nucmer_commands(files)
-    assert_equal(cmdlist, ['nucmer --mum -p ./nucmer_output/file1_vs_file2 ' +
-                           'file1 file2; delta-filter -1 ' +
-                           './nucmer_output/file1_vs_file2.delta > ' +
-                           './nucmer_output/file1_vs_file2.filter',
-                           'nucmer --mum -p ./nucmer_output/file1_vs_file3 ' +
-                           'file1 file3; delta-filter -1 ' +
-                           './nucmer_output/file1_vs_file3.delta > ' +
-                           './nucmer_output/file1_vs_file3.filter',
-                           'nucmer --mum -p ./nucmer_output/file1_vs_file4 ' +
-                           'file1 file4; delta-filter -1 ' +
-                           './nucmer_output/file1_vs_file4.delta > ' +
-                           './nucmer_output/file1_vs_file4.filter',
-                           'nucmer --mum -p ./nucmer_output/file2_vs_file3 ' +
-                           'file2 file3; delta-filter -1 ' +
-                           './nucmer_output/file2_vs_file3.delta > ' +
-                           './nucmer_output/file2_vs_file3.filter',
-                           'nucmer --mum -p ./nucmer_output/file2_vs_file4 ' +
-                           'file2 file4; delta-filter -1 ' +
-                           './nucmer_output/file2_vs_file4.delta > ' +
-                           './nucmer_output/file2_vs_file4.filter',
-                           'nucmer --mum -p ./nucmer_output/file3_vs_file4 ' +
-                           'file3 file4; delta-filter -1 ' +
-                           './nucmer_output/file3_vs_file4.delta > ' +
-                           './nucmer_output/file3_vs_file4.filter'])    
-    print(cmdlist)
+    ncmds, fcmds = anim.generate_nucmer_commands(files)
+    print('\n'.join([ncmd for ncmd in ncmds]))
+    print('\n'.join([fcmd for fcmd in fcmds]))
+    assert_equal(ncmds, ['nucmer --mum -p ./nucmer_output/file1_vs_file2 file1 file2',
+                         'nucmer --mum -p ./nucmer_output/file1_vs_file3 file1 file3',
+                         'nucmer --mum -p ./nucmer_output/file1_vs_file4 file1 file4',
+                         'nucmer --mum -p ./nucmer_output/file2_vs_file3 file2 file3',
+                         'nucmer --mum -p ./nucmer_output/file2_vs_file4 file2 file4',
+                         'nucmer --mum -p ./nucmer_output/file3_vs_file4 file3 file4'])
+    assert_equal(fcmds, ['delta_filter_wrapper.py delta-filter -1 ' +
+                         './nucmer_output/file1_vs_file2.delta ' +
+                         './nucmer_output/file1_vs_file2.filter',
+                         'delta_filter_wrapper.py delta-filter -1 ' +
+                         './nucmer_output/file1_vs_file3.delta ' +
+                         './nucmer_output/file1_vs_file3.filter',
+                         'delta_filter_wrapper.py delta-filter -1 ' +
+                         './nucmer_output/file1_vs_file4.delta ' +
+                         './nucmer_output/file1_vs_file4.filter',
+                         'delta_filter_wrapper.py delta-filter -1 ' +
+                         './nucmer_output/file2_vs_file3.delta ' +
+                         './nucmer_output/file2_vs_file3.filter',
+                         'delta_filter_wrapper.py delta-filter -1 ' +
+                         './nucmer_output/file2_vs_file4.delta ' +
+                         './nucmer_output/file2_vs_file4.filter',
+                         'delta_filter_wrapper.py delta-filter -1 ' +
+                         './nucmer_output/file3_vs_file4.delta ' +
+                         './nucmer_output/file3_vs_file4.filter'])
