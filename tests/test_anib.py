75e9d54eead267c81bb6196396ffdca05c5637a9
Leighton Pritchard <leighton.pritchard@hutton.ac.uk>
2017-11-22 15:12:53 +0000

    sort test outputs for Python3.5 tests

--- tests/test_anib.py
+++ tests/test_anib.py
@@ -148,35 +148,36 @@ class TestBLASTCmdline(unittest.TestCase):
                                       '-i tests/test_input/sequences/NC_010338.fna',
                                       '-d tests/test_input/sequences/NC_002696.fna',
                                       '-X 150 -q -1 -F F -e 1e-15 -b 1 -v 1 -m 8'])]
-        self.blastnjobdict = [('tests/test_output/anib/NC_002696.fna',
-                               'makeblastdb -dbtype nucl ' +
-                               '-in tests/test_input/sequences/NC_002696.fna ' +
-                               '-title NC_002696 -out tests/test_output/anib/NC_002696.fna'),
-                              ('tests/test_output/anib/NC_010338.fna',
-                               'makeblastdb -dbtype nucl ' +
-                               '-in tests/test_input/sequences/NC_010338.fna ' +
-                               '-title NC_010338 -out tests/test_output/anib/NC_010338.fna'),
-                              ('tests/test_output/anib/NC_011916.fna',
-                               'makeblastdb -dbtype nucl ' +
-                               '-in tests/test_input/sequences/NC_011916.fna ' +
-                               '-title NC_011916 -out tests/test_output/anib/NC_011916.fna'),
-                              ('tests/test_output/anib/NC_014100.fna',
-                               'makeblastdb -dbtype nucl ' +
-                               '-in tests/test_input/sequences/NC_014100.fna ' +
-                               '-title NC_014100 -out tests/test_output/anib/NC_014100.fna')]
-        self.blastalljobdict =  [('tests/test_output/anib/NC_002696.fna',
-                                  'formatdb -p F -i tests/test_output/anib/NC_002696.fna ' +
-                                  '-t NC_002696'),
-                                 ('tests/test_output/anib/NC_010338.fna',
-                                  'formatdb -p F -i tests/test_output/anib/NC_010338.fna ' +
-                                  '-t NC_010338'),
-                                 ('tests/test_output/anib/NC_011916.fna',
-                                  'formatdb -p F -i tests/test_output/anib/NC_011916.fna ' +
-                                  '-t NC_011916'),
-                                 ('tests/test_output/anib/NC_014100.fna',
-                                  'formatdb -p F -i tests/test_output/anib/NC_014100.fna ' +
-                                  '-t NC_014100')]
-        self.blastngraphtgt = []
+        self.blastnjobdict = sorted(
+            [('tests/test_output/anib/NC_002696.fna',
+              'makeblastdb -dbtype nucl ' +
+              '-in tests/test_input/sequences/NC_002696.fna ' +
+              '-title NC_002696 -out tests/test_output/anib/NC_002696.fna'),
+             ('tests/test_output/anib/NC_010338.fna',
+              'makeblastdb -dbtype nucl ' +
+              '-in tests/test_input/sequences/NC_010338.fna ' +
+              '-title NC_010338 -out tests/test_output/anib/NC_010338.fna'),
+             ('tests/test_output/anib/NC_011916.fna',
+              'makeblastdb -dbtype nucl ' +
+              '-in tests/test_input/sequences/NC_011916.fna ' +
+              '-title NC_011916 -out tests/test_output/anib/NC_011916.fna'),
+             ('tests/test_output/anib/NC_014100.fna',
+              'makeblastdb -dbtype nucl ' +
+              '-in tests/test_input/sequences/NC_014100.fna ' +
+              '-title NC_014100 -out tests/test_output/anib/NC_014100.fna')])
+        self.blastalljobdict = sorted(
+            [('tests/test_output/anib/NC_002696.fna',
+              'formatdb -p F -i tests/test_output/anib/NC_002696.fna ' +
+              '-t NC_002696'),
+             ('tests/test_output/anib/NC_010338.fna',
+              'formatdb -p F -i tests/test_output/anib/NC_010338.fna ' +
+              '-t NC_010338'),
+             ('tests/test_output/anib/NC_011916.fna',
+              'formatdb -p F -i tests/test_output/anib/NC_011916.fna ' +
+              '-t NC_011916'),
+             ('tests/test_output/anib/NC_014100.fna',
+              'formatdb -p F -i tests/test_output/anib/NC_014100.fna ' +
+              '-t NC_014100')])
         os.makedirs(self.outdir, exist_ok=True)
         os.makedirs(self.fmtdboutdir, exist_ok=True)
         os.makedirs(self.makeblastdbdir, exist_ok=True)
@@ -237,14 +238,14 @@ class TestBLASTCmdline(unittest.TestCase):
                    os.listdir(self.seqdir)]
         blastcmds = anib.make_blastcmd_builder("ANIblastall", self.outdir)
         jobdict = anib.build_db_jobs(self.infiles, blastcmds)
-        assert_equal([(k, v.script) for (k, v) in jobdict.items()],
+        assert_equal(sorted([(k, v.script) for (k, v) in jobdict.items()]),
                      self.blastalljobdict)
 
     def test_blastn_dbjobdict(self):
         """generate dictionary of BLASTN+ database jobs."""
         blastcmds = anib.make_blastcmd_builder("ANIb", self.outdir)
         jobdict = anib.build_db_jobs(self.infiles, blastcmds)
-        assert_equal([(k, v.script) for (k, v) in jobdict.items()],
+        assert_equal(sorted([(k, v.script) for (k, v) in jobdict.items()]),
                      self.blastnjobdict)
 
     def test_blastn_graph(self):
