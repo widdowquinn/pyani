0188e243066b9c5f8aa4c20c1f791473d31faa31
Leighton Pritchard <leighton.pritchard@hutton.ac.uk>
2017-11-22 17:10:01 +0000

    update test module headers for multiprocessing

--- tests/test_multiprocessing.py
+++ tests/test_multiprocessing.py
@@ -1,21 +1,63 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-"""Tests for pyani package multiprocessing runs
+"""test_multiprocessing.py
 
-These tests are intended to be run using the nose package
-(see https://nose.readthedocs.org/en/latest/).
+Test run_multiprocessing.py module.
+
+These tests are intended to be run from the repository root using:
+
+nosetests -v
+
+print() statements will be caught by nosetests unless there is an
+error. They can also be recovered with the -s option.
+
+(c) The James Hutton Institute 2017
+Author: Leighton Pritchard
+
+Contact:
+leighton.pritchard@hutton.ac.uk
+
+Leighton Pritchard,
+Information and Computing Sciences,
+James Hutton Institute,
+Errol Road,
+Invergowrie,
+Dundee,
+DD6 9LH,
+Scotland,
+UK
+
+The MIT License
+
+Copyright (c) 2017 The James Hutton Institute
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
 """
 
+import unittest
 
-from nose.tools import assert_equal
-from pyani import run_multiprocessing
+from pyani import (run_multiprocessing, )
 
 
-# Test ANIm command-lines
-# One pairwise comparison
 def test_multiprocessing_run():
-    """Test basic multiprocessing function
-    """
+    """Test basic multiprocessing function."""
     cmdlist = ['for i in %s; do echo "Thread %d: value ${i}"; done' %
                (' '.join([str(e) for e in range(v)]), v) for
                v in range(5)]
