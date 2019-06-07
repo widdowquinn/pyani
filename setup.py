953adebe6d85042c27e17c51edc10fc2c7608c70
Leighton Pritchard <leighton.pritchard@hutton.ac.uk>
2019-06-07 18:37:50 +0100

    add docstring and copyright message to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,52 @@
-# try using distribute or setuptools or distutils.
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""setup.py
+
+Distribution setup
+
+(c) The James Hutton Institute 2017-2019
+
+Author: Leighton Pritchard
+Contact: leighton.pritchard@hutton.ac.uk
+
+Leighton Pritchard,
+Information and Computing Sciences,
+James Hutton Institute,
+Errol Road,
+Invergowrie,
+Dundee,
+DD6 9LH,
+Scotland,
+UK
+
+The MIT License
+
+Copyright (c) 2017-2019 The James Hutton Institute
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
+import os
+import sys
+import re
+import setuptools
+
 try:
     import distribute_setup
 
@@ -6,15 +54,9 @@ try:
 except ImportError:
     pass
 
-import setuptools
-
-import os
-import sys
-import re
-
 # Get long description from README.md
 with open("README.md", "r") as dfh:
-    long_description = dfh.read()
+    long_description = dfh.read()  # pylint: disable=C0103
 
 # parse version from package/module without importing or
 # evaluating the code
