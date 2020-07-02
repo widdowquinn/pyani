597a9bef9f99eb085534fd0ddf1ec70d3d30d080
Leighton Pritchard <leighton.pritchard@mac.com>
2020-07-02 10:46:37 +0100

    record requirement for Python >= 3.7 in setup.py

--- setup.py
+++ setup.py
@@ -122,9 +122,8 @@ setuptools.setup(
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
     ],
 )
