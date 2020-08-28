a658900f27edb6920ea6a018675d92b2f666a7cc
peterjc <p.j.a.cock@googlemail.com>
2020-08-28 16:47:51 +0100

    Allow reference genome FASTA files to be symlinks

--- pyani/pyani_orm.py
+++ pyani/pyani_orm.py
@@ -571,7 +571,7 @@ def add_run_genomes(
             indesc = read_fasta_description(fastafile)
         except Exception:
             raise PyaniORMException("Could not read genome files for database import")
-        abspath = fastafile.resolve()
+        abspath = fastafile.absolute()
         genome_len = get_genome_length(abspath)
         # If the genome is not already in the database, add it as a Genome object
         genome = session.query(Genome).filter(Genome.genome_hash == inhash).first()
