6d7b9385408e739573a3dab9e7256b982fbc5d9f
baileythegreen <baileythegreen@gmail.com>
2021-06-01 17:37:33 +0100

    Filled out empty function bodies
    Wrote bodies for `generate_fastani_jobs()`, 
    `generate_fastani_commands()`, `construct_fastani_cmdline()`, and 
    `process_files()`.

--- pyani/fastani.py
+++ pyani/fastani.py
@@ -50,6 +50,7 @@ from Bio import SeqIO
 
 from . import pyani_config
 from . import pyani_files
+from . import pyani_jobs
 from .pyani_tools import ANIResults, BLASTcmds, BLASTexes, BLASTfunctions
 
 
@@ -87,23 +88,98 @@ def get_version(fastani_exe: Path = pyani_config.FASTANI_DEFAULT) -> str:
 
 # Generate list of Job objects, one per fastANI run -> this is maybe not necessary
 def generate_fastani_jobs(
-    reference: Path,
-    query: Path,
+    filenames: List[Path],
     outdir: Path = Path("."),
     fastani_exe: Path = pyani_config.FASTANI_DEFAULT,
-    # kmer
-):
-    pass
+    kmer: int = 16,
+    fragLen: int = 3000,
+    minFraction: float = 0.2,
+    jobprefix: str = "fastANI",
+):  # should this have a hint for return type?
+    """Return list of Jobs describing fastANI command lines.
+
+    :param filenames:   a list of paths to input FASTA files
+    :param outdir:      path to output directory
+    :param fastani_exe: location of the fastANI binary
+    :param kmer:        kmer size to use
+    :param fragLen:     fragment length to use
+    :param minFraction: minimum portion of the genomes that must match to trust ANI
+    :param jobprefix:
+
+    Loop over all FASTA files, generating Jobs describing fastANI command lines
+    for each pairwise comparison.
+    """
+    fastcmds = generate_fastani_commands(
+        filenames, outdir, fastani_exe, kmer, fragLen, minFraction
+    )
+    joblist = []
+    for idx, fastcmd in enumerate(fastcmds):
+        fastjob = pyani_jobs.Job(f"{jobprefix}_{idx:06d}", fastcmd)
+        joblist.append(fastjob)
+    return joblist
 
 
-def generate_commands():
-    pass
+def generate_fastani_commands(
+    filenames: List[Path],
+    outdir: Path = Path("."),
+    fastani_exe: Path = pyani_config.FASTANI_DEFAULT,
+    kmer: int = 16,
+    fragLen: int = 3000,
+    minFraction: float = 0.2,
+) -> List[str]:
+    """Return list of fastANI command lines.
+
+    :param filenames:   a list of paths to input FASTA files
+    :param outdir:      path to output directory
+    :param fastani_exe: location of the fastANI binary
+    :param kmer:        kmer size to use
+    :param fragLen:     fragment length to use
+    :param minFraction: minimum portion of the genomes that must match to trust ANI
+
+    Loop over all FASTA files generating fastANI command lines for each pairwise comparison.
+    """
+    fastani_cmdlines = []
+    filenames = sorted(filenames)  # enforce ordering of filenames
+    for idx, query in enumerate(filenames):
+        for index, ref in enumerate(filenames):
+            if idx == index:
+                pass  # do we want to compare things to themselves?
+            fastcmd = construct_fastani_cmdline(
+                query, ref, outdir, fastani_exe, kmer, fragLen, minFraction
+            )
+            fastani_cmdlines.append(fastcmd)
+    return fastani_cmdlines
 
 
-def construct_fastani_cmdline():
-    """Will return a fastcmd item"""
-    # return fastcmd
-    pass
+# ¶ If int type is specified here, can a value of None be passed?
+# ¶ I have added default values because I am not sure.
+# ¶ This currently does not use the list option of fastANI
+def construct_fastani_cmdline(
+    query: Path,
+    ref: Path,
+    outdir: Path = Path("."),
+    fastani_exe: Path = pyani_config.FASTANI_DEFAULT,
+    kmer: int = 16,
+    fragLen: int = 3000,
+    minFraction: float = 0.2,
+) -> str:
+    """Will return a fastcmd item
+
+    :param query:           path to query file
+    :param ref:             path to reference file
+    :param outdir:          path to output directory
+    :param fastani_exe:     path to fastANI executable
+    :param kmer:            kmer size to use
+    :param fragLen:         fragment length to use
+    :param minFraction:     minimum portion of the genomes that must match to trust ANI
+    """
+    # Cast path strings to pathlib.Path for safety
+    query, ref = Path(query), Path(ref)
+
+    # Compile commands
+    outprefix = outdir / f"{query.stem}_vs_{ref.stem}"
+    fastcmd = f"{fastani_exe} -q {query} -r {ref} -o {outprefix} -k {kmer} --fragLen {fragLen} --minFraction {minFraction}"
+    return fastcmd
 
 
 def parse_output_file(filename: Path) -> ComparisonResult:
@@ -120,21 +196,107 @@ def parse_output_file(filename: Path) -> ComparisonResult:
     # def add_snp(holder, type, key, *value):
     #    holder[key] = type(*value)
     # Create some sort of holder:
-    box = []
+    # ¶ The following is for an output file with multiple lines
+    results = []
     for line in [_.strip().split() for _ in open(filename, "r").readlines()]:
         if len(line) == 5:
-            box.append(ComparisonResult(*line))
+            results.append(ComparisonResult(*line))
         else:
-            raise ValueError("Line contains too many/too few items: %s" % line)
+            raise ValueError(f"Line contains too many/too few items: {line}")
             continue
-    return box
+    return results
 
 
-def process_files():
-    pass
+def process_files(
+    outdir: Path, org_lengths: Dict, logger: Optional[Logger] = None
+) -> ANIResults:
+    """Return tuple of fastANI results for files in passed directory.
+
+    :param outdir:  Path, path to the directory containing output files
+    :param org_lengths: dictionary of total sequence lengths, keyed by sequence
+
+    Returns the following pandas dataframes in an ANIResults object;
+    query sequences are rows, reference sequences are columns:
+
+    - alignment_lengths - asymmetrical: total length of alignment
+    - percentage_identity - asymmetrical: percentage identity of alignment
+    - alignment_coverage - asymmetrical: coverage of query and reference
+    - similarity_errors - asymmetrical: count of similarity errors
+
+    May throw a ZeroDivisionError if one or more fastANI runs failed, or a
+    very distant sequence was included in the analysis.
+    """
+    # Process directory to identify input files
+    outfiles = pyani_files.get_input_files(outdir, ".out")
+
+    # Hold data in ANIResults object
+    results = ANIResults(list(org_lengths.keys()), "fastANI")
+
+    # Fill diagonal NA values for alignment_length with org_lengths
+    # ¶ Is this necessary? Or can ANIm not do org X org comparisons? Will the ANIResults object always have NAs on the diagonal?
+    for org, length in list(org_lengths.items()):
+        results.alignment_lengths[org][org] = length
+
+    # Process .out files assuming that the filename format holds:
+    # org1_vs_org2.out
+    for outfile in outfiles:
+        qname, rname = outfile.stem.split("_vs_")
+
+        # We may have .out files from other analyses int eh same directory
+        # If this occurs, we raise a warning and skip the .out file
+        if qname not in list(org_lengths.keys()):
+            if logger:
+                logger.warning(
+                    "Query name %s not in input sequence list, skipping %s",
+                    qname,
+                    outfile,
+                )
+            continue
+        if rname not in list(org_lengths.keys()):
+            if logger:
+                logger.warning(
+                    "Reference name %s not in input sequence list, skipping %s",
+                    rname,
+                    outfile,
+                )
+            continue
+        resultList = parse_output_file(
+            outfile
+        )  # Returns a list of ComparisonResult objects
+        for result in resultList:
+            if result.orthologs == 0 and logger is not None:
+                if logger:
+                    logger.warning(
+                        "Total alignment length reported in %s is zero!", outfile
+                    )
+            tot_length = result.orthologs
+            sim_errors = result.fragments - tot_length
+            query_cover = float(tot_length) / org_lengths[qname]
+
+            # Calculate percentage ID of aligned length. This may fail if
+            # total length is zero.
+            # The ZeroDivisionError that would arise should be handled
+            # Common causes are that a NUCmer run failed, or that a very
+            # distant sequence was included in the analysis.
+            try:
+                perc_id = 1 - float(sim_errors) / tot_length
+            except ZeroDivisionError:
+                perc_id = 0  # set arbitrary value of zero identity
+                results.zero_error = True
+            results.add_tot_length(qname, rname, tot_length)
+            results.add_sim_errors(qname, rname, sim_errors)
+            results.add_pid(qname, rname, perc_id)
+            results.add_coverage(qname, rname, query_cover, None)
+    return results
 
 
 # """
+# class ComparisonResult(NamedTuple):
+#    reference: Path
+#    query: Path
+#    ani: float
+#    orthologs: int
+#    fragments: int
 # fastANI is a fast alignment-free implementation for computing whole-genome
 # Average Nucleotide Identity (ANI) between genomes
 # -----------------
