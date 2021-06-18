fd59449bc0fa03602832817e0b314acf0d2b9033
widdowquinn <leighton.pritchard@mac.com>
2021-06-18 15:03:22 +0100

    update copyright in pyani_jobs.py

--- pyani/pyani_jobs.py
+++ pyani/pyani_jobs.py
@@ -17,7 +17,7 @@
 # The MIT License
 #
 # Copyright (c) 2013-2019 The James Hutton Institute
-# Copyright (c) 2019 University of Strathclyde
+# Copyright (c) 2019-2021 University of Strathclyde
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
