0e3d471c4c52a07ab3bf6d29f95857d8c2588ba0
baileythegreen <baileythegreen@gmail.com>
2021-09-14 00:35:36 +0100

    Add `get_version()` tests and boilerplate

--- tests/test_aniblastall.py
+++ tests/test_aniblastall.py
@@ -1,7 +1,42 @@
-"""Test anib.py module.
-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# (c) University of Strathclyde 2019-2021
+# Author: Bailey Harrington
+#
+# Contact:
+# leighton.pritchard@strath.ac.uk
+#
+# Leighton Pritchard,
+# Strathclyde Institute for Pharmacy and Biomedical Sciences,
+# 161 Cathedral Street,
+# Glasgow,
+# G4 0RE
+# Scotland,
+# UK
+#
+# The MIT License
+#
+# Copyright (c) 2019-2021 University of Strathclyde
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+"""Test aniblastall.py module.
 These tests are intended to be run from the repository root using:
-
 pytest -v
 """
 
@@ -14,6 +49,7 @@ import pytest  # noqa: F401  # pylint: disable=unused-import
 from pandas.util.testing import assert_frame_equal
 
 from pyani import anib, pyani_files
+from pyani import aniblastall
 
 
 class ANIbOutput(NamedTuple):
@@ -78,6 +114,45 @@ def anib_output_dir(dir_anib_in):
         dir_anib_in / "blastall",
         pd.read_csv(dir_anib_in / "dataframes" / "blastn_result.csv", index_col=0),
         pd.read_csv(dir_anib_in / "dataframes" / "blastall_result.csv", index_col=0),
+    )
+
+
+# Test get_version()
+# Test case 1: there is no executable
+def test_get_version_missing_exe(executable_missing):
+    """Test behaviour when there is no file at the specified executable location."""
+    test_file_1 = Path("/non/existent/blastall")
+    assert aniblastall.get_version(test_file_1) == f"No blastall at {test_file_1}"
+
+
+# Test case 2: there is a file, but it is not executable
+def test_get_version_not_executable(executable_not_executable):
+    """Test behaviour when the file at the executable location is not executable."""
+    test_file_2 = Path("/non/executable/blastall")
+    assert (
+        aniblastall.get_version(test_file_2)
+        == f"blastall exists at {test_file_2} but not executable"
+    )
+
+
+# Test case 3: there is an executable file, but the version can't be retrieved
+def test_get_version_no_version(executable_without_version):
+    """Test behaviour when the version for the executable can not be retrieved."""
+    test_file_3 = Path("/missing/version/blastall")
+    assert (
+        aniblastall.get_version(test_file_3)
+        == f"blastall exists at {test_file_3} but could not retrieve version"
+    )
+
+
+# Test case 4: there is an executable file, but it will not run on the OS
+def test_get_version_os_incompatible(executable_incompatible_with_os):
+    """Test behaviour when the program can't run on the operating system.
+    This will happen with newer versions of MacOS."""
+    test_file_4 = Path("/os/incompatible/blastall")
+    assert (
+        aniblastall.get_version(test_file_4)
+        == f"blastall exists at {test_file_4} but could not be executed"
     )
 
 
