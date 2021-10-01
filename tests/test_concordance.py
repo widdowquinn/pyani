25cb60ab423f5371ea149cc015c0b0a0c0bef808
baileythegreen <baileythegreen@gmail.com>
2021-10-01 14:59:16 +0100

    Add debugging lines for concordance test

--- tests/test_concordance.py
+++ tests/test_concordance.py
@@ -47,6 +47,8 @@ print() statements will be caught by nosetests unless there is an
 error. They can also be recovered with the -s option.
 """
 
+import sys
+
 import unittest
 
 from pathlib import Path
@@ -200,6 +202,8 @@ def test_anim_concordance(
     result_pid = (result_pid.sort_index(axis=0).sort_index(axis=1) * 100.0).values
     tgt_pid = parse_jspecies(path_concordance_jspecies)["ANIm"].values
 
+    sys.stdout.write(f"Result: {result_pid - tgt_pid}\n")
+    sys.stdout.write(f"Expected: {pytest.approx(0, abs=tolerance_anim)}")
     assert result_pid - tgt_pid == pytest.approx(0, abs=tolerance_anim)
 
 
