289d638607b1cda239891c86836a19a7a24fbe9e
baileythegreen <baileythegreen@gmail.com>
2021-10-08 13:43:10 +0100

    Add files for `pyani compare`

---
+++ pyani/scripts/parsers/compare_parser.py
@@ -0,0 +1,50 @@
+"""Provides parser for the compare subcommand."""
+
+from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser, _SubParsersAction
+from pathlib import Path
+from typing import List, Optional
+
+from pyani.scripts import subcommands
+
+
+def build(
+    subps: _SubParsersAction, parents: Optional[List[ArgumentParser]] = None
+) -> None:
+    """Return a command-line parser for the plot subcommand.
+
+    :param subps:  collectin of subparsers in main parser
+    :param parents:  parsers from which arguments are inherited
+
+    The compare subcommand takes specific arguments:
+
+    """
+    parser = subps.add_parser(
+        "compare", parents=parents, formatter_class=ArgumentDefaultsHelpFormatter
+    )
+    # Required arguments: output directory and run IDs
+    parser.add_argument(
+        "-o",
+        "--outdir",
+        action="store",
+        dest="outdir",
+        type=Path,
+        required=True,
+        help="output directory",
+    )
+    parser.add_argument(
+        "--run_id",
+        action="store",
+        dest="run_id",
+        require=True,
+        help="run IDs to compare",
+    )
+    # Optional arguments
+    parser.add_argument(
+        "--dbpath",
+        action="store",
+        dest="dbpath",
+        default=Path(".pyani/pyanidb"),
+        type=Path,
+        help="path to pyani database",
+    )
+    parser.set_defaults(func=subcommands.subcmd_compare)
