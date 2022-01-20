266d823807781cb30ca565b7c94ae20d4875fb3b
widdowquinn <leightonpritchard@mac.com>
2022-01-20 15:57:20 +0000

    suppress warning about SQL JOIN statements arising from pandas

--- tests/test_subcmd_05_report.py
+++ tests/test_subcmd_05_report.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # (c) The James Hutton Institute 2017-2019
-# (c) The University of Strathclude 2019
+# (c) The University of Strathclude 2019-2022
 # Author: Leighton Pritchard
 #
 # Contact:
@@ -19,7 +19,7 @@
 # The MIT License
 #
 # Copyright (c) 2017-2018 The James Hutton Institute
-# (c) The University of Strathclude 2019
+# (c) The University of Strathclude 2019-2022
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
