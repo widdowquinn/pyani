9c642b39fb450a2a46157bebca2d61692a6cfb3b
baileythegreen <baileythegreen@gmail.com>
2022-03-01 19:10:31 -1000

    Update migration file to allow for reflection in offline mode

--- alembic/versions/92f7f6b1626e_add_fastani_columns.py
+++ alembic/versions/92f7f6b1626e_add_fastani_columns.py
@@ -8,6 +8,17 @@ Create Date: 2022-02-07 22:57:35.779356
 from alembic import op
 import sqlalchemy as sa
 
+from sqlalchemy import (
+    Column,
+    Table,
+    MetaData,
+    ForeignKey,
+    Integer,
+    String,
+    Float,
+    Boolean,
+    UniqueConstraint,
+)
 
 # revision identifiers, used by Alembic.
 revision = "92f7f6b1626e"
@@ -16,11 +27,69 @@ branch_labels = None
 depends_on = None
 
 
+meta = MetaData()
+new_comparisons = Table(
+    "new_comparisons",
+    meta,
+    Column("comparisons_id", Integer, primary_key=True),
+    Column("query_id", Integer, ForeignKey("genomes.genome_id"), nullable=False),
+    Column("subject_id", Integer, ForeignKey("genomes.genome_id"), nullable=False),
+    Column("aln_length", Integer),
+    Column("sim_errs", Integer),
+    Column("identity", Float),
+    Column("cov_query", Float),
+    Column("cov_subject", Float),
+    Column("program", String),
+    Column("version", String),
+    Column("fragsize", Integer),
+    Column("maxmatch", Boolean),
+    Column("kmersize", Integer),
+    Column("minmatch", Float),
+    UniqueConstraint(
+        "query_id",
+        "subject_id",
+        "program",
+        "version",
+        "fragsize",
+        "maxmatch",
+        "kmersize",
+        "minmatch",
+        name="fastani_reqs",
+    ),
+)
+
+old_comparisons = Table(
+    "old_comparisons",
+    meta,
+    Column("comparisons_id", Integer, primary_key=True),
+    Column("query_id", Integer, ForeignKey("genomes.genome_id"), nullable=False),
+    Column("subject_id", Integer, ForeignKey("genomes.genome_id"), nullable=False),
+    Column("aln_length", Integer),
+    Column("sim_errs", Integer),
+    Column("identity", Float),
+    Column("cov_query", Float),
+    Column("cov_subject", Float),
+    Column("program", String),
+    Column("version", String),
+    Column("fragsize", Integer),
+    Column("maxmatch", Boolean),
+    UniqueConstraint(
+        "query_id",
+        "subject_id",
+        "program",
+        "version",
+        "fragsize",
+        "maxmatch",
+        name="base_reqs",
+    ),
+)
+
+
 def upgrade():
     # op.add_column("comparisons", sa.Column("kmersize", sa.Integer))
     # op.add_column("comparisons", sa.Column("minmatch", sa.Float))
 
-    with op.batch_alter_table("comparisons") as batch_op:
+    with op.batch_alter_table("comparisons", copy_from=old_comparisons) as batch_op:
         batch_op.add_column(sa.Column("kmersize", sa.Integer))
         batch_op.add_column(sa.Column("minmatch", sa.Float))
         batch_op.create_unique_constraint(
@@ -42,7 +111,7 @@ def downgrade():
     # op.drop_constraint("comparisons", 'kmersize')
     # op.drop_column("comparisons", "kmersize")
 
-    with op.batch_alter_table("comparisons") as batch_op:
+    with op.batch_alter_table("comparisons", copy_from=new_comparisons) as batch_op:
         batch_op.drop_column("kmersize")
         batch_op.drop_column("minmatch")
         batch_op.drop_constraint("fastani_reqs")
