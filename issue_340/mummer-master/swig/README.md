724510b59960535be71d48acb6b029d875191c36
Leighton Pritchard <leightonpritchard@mac.com>
2024-04-08 16:53:35 +0100

    removed issue_340 and issue_421 folders from the PR
    Support files for specific issues that are not part of the codebase
    should not be included in the pull request.

--- issue_340/mummer-master/swig/README.md
+++
@@ -1,7 +0,0 @@
-  # SWIG bindings
-  
-  For installation instructions, see the [SWIG installation guide](swig/INSTALL.md).
-  
-  ## Examples
-  
-  Soon.
--- issue_421/rounding_error/scripts/01_run_analyses.sh
+++
@@ -1,5 +0,0 @@
-# # Running DNADIFF comparisions
-# dnadiff ../data/donovan_test/input/MGV-GENOME-0264574.fna ../data/donovan_test/input/MGV-GENOME-0266457.fna -p ../data/donovan_test/output/dnadiff/output
-# dnadiff ../data/donovan_AF_bug/input/MGV-GENOME-0357962.fna ../data/donovan_AF_bug/input/MGV-GENOME-0358017.fna -p ../data/donovan_AF_bug/output/dnadiff/output
-dnadiff ../data/streptomyces_genomes/input/GCF_002802945.1_ASM280294v1_genomic.fna ../data/streptomyces_genomes/input/GCF_003369795.1_ASM336979v1_genomic.fna -p ../data/streptomyces_genomes/output/dnadiff/output
-
